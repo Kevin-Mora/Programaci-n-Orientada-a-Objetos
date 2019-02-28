881a30fd7fb148cd01199fa70c8ee30ca0724540
Kevin-Mora <aikonx4_dantex4@hotmail.com>
2019-02-27 19:29:10 -0800

    creacion de tarea2

---
+++ Tareapoo2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tareapoo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
