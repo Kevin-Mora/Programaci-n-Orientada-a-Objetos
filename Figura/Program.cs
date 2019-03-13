627c13ad0ba570aea17b4781b2ae4129afbe1d6c
Kevin-Mora <aikonx4_dantex4@hotmail.com>
2019-03-12 20:54:08 -0700

    Figura

---
+++ Figura/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Figura2
+{
+    class Vector2d
+    {
+        public int x, y;
+        public Vector2d(int x, int y)
+        {
+            this.x=x; this.y=y;
+        }
+        public override string ToString()
+        {
+            return String.Format("{0},{1}", x, y);
+        }
+    }
+    abstract class Figura
+    {
+        public Vector2d position;
+        public string fill ,border;
+
+          //Constructor por defecto 
+        public Figura():this( new Vector2d(100, 100))
+        {
+        
+        }
+        //constructor de figura
+        public Figura(Vector2d pos)
+        {
+            position= pos;
+            fill= "white";
+            border= "black";
+        }
+
+        public abstract void Dibuja();
+    }
+
+    class Circulo : Figura
+    {
+     private int radio;
+     public Circulo(Vector2d pos, int radio):base(pos)
+     {
+         this.radio= radio;
+     }
+     public Circulo ():base()
+     {
+         this.radio= 10;
+     }
+
+     public override void Dibuja() 
+     {
+         Console.WriteLine("Se dibuja un circulo en {0} de color {1}", position, fill);
+     }
+    }
+ 
+    class Rectangulo : Figura
+    {
+     
+     public Rectangulo(Vector2d pos):base(pos)
+     {
+         
+     }
+     public Rectangulo ():base()
+     {
+        
+     }
+
+     public override void Dibuja() 
+     {
+         Console.WriteLine("Se dibuja un Rectangulo en {0} de color {1}", position, fill);
+     }
+    }
+
+    class Cuadrado : Figura
+    {
+     
+     public Cuadrado(Vector2d pos):base(pos)
+     {
+         
+     }
+     public Cuadrado ():base()
+     {
+        
+     }
+
+     public override void Dibuja() 
+     {
+         Console.WriteLine("Se dibuja un Cuadrado en {0} de color {1}", position, fill);
+     }
+    }
+
+     class Triangulo : Figura
+    {
+     
+     public Triangulo(Vector2d pos):base(pos)
+     {
+         
+     }
+     public Triangulo ():base()
+     {
+        
+     }
+
+     public override void Dibuja() 
+     {
+         Console.WriteLine("Se dibuja un Triangulo en {0} de color {1}", position, fill);
+     }
+    }
+
+    class Decagono : Figura
+    {
+     
+     public Decagono(Vector2d pos):base(pos)
+     {
+         
+     }
+     public Decagono ():base()
+     {
+        
+     }
+
+     public override void Dibuja() 
+     {
+         Console.WriteLine("Se dibuja un Decagono en {0} de color {1}", position, fill);
+     }
+    }
+
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Figura> figuras = new List<Figura>();
+            figuras.Add(new Circulo());
+            figuras.Add(new Rectangulo(new Vector2d(200,200) ));
+            figuras.Add(new Cuadrado(new Vector2d(150,200) ));
+            figuras.Add(new Triangulo(new Vector2d(150,250) ));
+            figuras.Add(new Decagono(new Vector2d(150,250) ));
+            foreach(Figura f in figuras)
+            f.Dibuja();
+           
+        }
+    }
+}
+
