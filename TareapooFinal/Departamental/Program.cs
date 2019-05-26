38e2d295b3868df5ae89a60c70dd52c6f60a9efc
Kevin-Mora <aikonx4_dantex4@hotmail.com>
2019-05-25 23:04:48 -0700

    pooFinal

---
+++ TareapooFinal/Departamental/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Departamental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduza número de departamento");
+            int num=1;
+            string captura;
+
+    
+            try
+            {
+                captura=Console.ReadLine();
+                num= Int32.Parse(captura);
+
+                if (num>5)
+                {
+                Console.WriteLine("El departamento no existe");
+                }
+    
+            }
+            catch( System.OverflowException)
+            {
+                Console.WriteLine("Overflow");
+            }
+            catch(Exception e)
+            {
+              Console.Write(e.Message);
+
+            }
+            finally
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
